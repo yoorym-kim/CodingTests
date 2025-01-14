8ca05af16487837d3179acb910d4fa3c22261786
yoorym kim <twilight99me@gmail.com>
2025-01-14 22:28:47 +0900

    [Bronze V] Title: A×B, Time: 68 ms, Memory: 11520 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line = br.readLine();
+        StringTokenizer st = new StringTokenizer(line, " ");
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        System.out.println(a * b);
+    }
+}
