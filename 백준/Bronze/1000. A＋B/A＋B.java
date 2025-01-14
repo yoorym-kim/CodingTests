94a4938fe586cd9e9bd63e35f6e008b5eb92c228
yoorym kim <twilight99me@gmail.com>
2025-01-14 21:48:21 +0900

    [Bronze V] Title: A+B, Time: 104 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -1,15 +1,13 @@
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String input = br.readLine();
-        StringTokenizer st = new StringTokenizer(input);
-
-        int A = Integer.parseInt(st.nextToken());
-        int B = Integer.parseInt(st.nextToken());
-
-        System.out.println(A + B);
-    }
+public class Main{
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String line = br.readLine();
+		StringTokenizer st = new StringTokenizer(line, " ");
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		System.out.println(a + b);
+	}	
 }
