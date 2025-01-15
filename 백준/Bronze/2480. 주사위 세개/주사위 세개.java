bea675ca906c9c568591a030847d7aaa8b5fa60f
yoorym kim <twilight99me@gmail.com>
2025-01-15 18:15:05 +0900

    [Bronze IV] Title: 주사위 세개, Time: 68 ms, Memory: 11504 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.java
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -11,18 +11,24 @@ public class Main{
         int b = Integer.parseInt(st.nextToken());
         int c = Integer.parseInt(st.nextToken());
 
-        if (a == b && a ==c){
-            System.out.println(10000+a*1000);
-        } else if (a != b && a != c && b != c) {
-            System.out.println(Math.max(a, Math.max(b, c))*100);
-        } else {
-            if (a == b){
+        if (a == b){
+            if (a == c){
+                System.out.println(10000+a*1000); //3개
+            }
+            else{
                 System.out.println(1000+a*100);
-            } else if (a == c) {
+            }
+        } else {   // a != b
+            if (a == c){
                 System.out.println(1000+a*100);
-            } else {
-                System.out.println(1000+b*100);
+            } else {   // a != c
+                if (b == c){
+                    System.out.println(1000+b*100);
+                } else {
+                    System.out.println(Math.max(a, Math.max(b, c))*100);
+                }
             }
         }
+
     }
 }
