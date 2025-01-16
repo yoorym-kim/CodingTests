9d4eb2487eb20c840143bd58b0309855fd70ab3d
yoorym kim <twilight99me@gmail.com>
2025-01-16 18:36:01 +0900

    [Bronze V] Title: 합, Time: 64 ms, Memory: 11552 KB -BaekjoonHub

--- 백준/Bronze/8393. 합/합.java
+++ 백준/Bronze/8393. 합/합.java
@@ -1,13 +1,10 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
-public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int sum = 0;
-        for (int i = 1; i <= n; i++) {
-           sum += i;
-        }
-        System.out.println(sum);
-    }
+public class Main{
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws Exception {
+		int n = Integer.parseInt(br.readLine());
+		System.out.println(n * (n + 1) / 2);
+	}
 }
