adc8a9aa804f47febfe9564761ddd51753469a24
yoorym kim <twilight99me@gmail.com>
2025-01-16 15:55:40 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 104 ms, Memory: 14136 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -5,11 +5,32 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
+        StringBuilder sb = new StringBuilder();
+        for(int i = 1; i <= n; i++) {
+            sb.append("*".repeat(Math.max(0, i)));
+            sb.append("\n");
+        }
+        System.out.println(sb);
+
+        /* StringBuilder로 이중for문
+        StringBuilder sb = new StringBuilder();
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= i; j++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+         */
+
+        /* 기본코드
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
             System.out.println();
         }
+        */
+
     }
 }
