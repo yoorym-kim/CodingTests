8d471a112e595ac4e142a4e31cd094de161bf34c
yoorym kim <twilight99me@gmail.com>
2025-01-20 18:04:51 +0900

    [Bronze I] Title: 평균, Time: 196 ms, Memory: 18564 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+
+        double average = Arrays.stream(arr).average().orElse(0);
+
+        System.out.println(average/max*100);
+    }
+}
