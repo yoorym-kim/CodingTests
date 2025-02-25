b49bd0c71dee79dc9f607da099687ee89fb1e435
yoorym kim <twilight99me@gmail.com>
2025-02-25 23:50:55 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 64 ms, Memory: 11568 KB -BaekjoonHub

---
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int V = Integer.parseInt(st.nextToken());
+
+        int days = (int) Math.ceil((double)(V - B) / (A - B));
+        System.out.println(days);
+    }
+}
