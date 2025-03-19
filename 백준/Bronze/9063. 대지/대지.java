a7ab0d816a1fc29f7b779e41d905a256cf0299d2
yoorym kim <twilight99me@gmail.com>
2025-03-19 16:49:36 +0900

    [Bronze III] Title: 대지, Time: 200 ms, Memory: 25568 KB -BaekjoonHub

---
+++ 백준/Bronze/9063. 대지/대지.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int minX = Integer.MAX_VALUE, minY = Integer.MAX_VALUE;
+        int maxX = Integer.MIN_VALUE, maxY = Integer.MIN_VALUE;
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            minX = Math.min(minX, x);
+            minY = Math.min(minY, y);
+            maxX = Math.max(maxX, x);
+            maxY = Math.max(maxY, y);
+        }
+
+        int width = maxX - minX;
+        int height = maxY - minY;
+
+        System.out.println(width * height);
+    }
+}
